2af96733d8c830b228e249ccb92d7380d1944d91
GORLI POORNA LAKSHMI PRANEETH <98073013+Praneethgpl@users.noreply.github.com>
2022-01-27 21:24:29 +0530

    #include<stdio.h> int main() {     int a,b,c;     scanf("%d%d",&a,&b);     if("1<=a<=10000&&1<=b<=10000")     {         c=a/b;         printf("%d",c);     }     return 0; }

---
+++ Division.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d",&a,&b);
+    if("1<=a<=10000&&1<=b<=10000")
+    {
+        c=a/b;
+        printf("%d",c);
+    }
+    return 0;
+}
